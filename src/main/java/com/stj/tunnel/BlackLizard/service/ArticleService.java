04c50bceb3e8d2aaea8574a08205c64a69486c2e
stj0215 <stj960215@gmail.com>
2021-01-25 21:05:23 +0900

    게시물 리스팅

--- src/main/java/com/stj/tunnel/BlackLizard/service/ArticleService.java
+++ src/main/java/com/stj/tunnel/BlackLizard/service/ArticleService.java
@@ -3,24 +3,18 @@ package com.stj.tunnel.BlackLizard.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.stj.tunnel.BlackLizard.dao.ArticleDao;
 import com.stj.tunnel.BlackLizard.dto.Article;
 
 @Service
 public class ArticleService {
+	@Autowired
+	private ArticleDao articleDao;
 	
-	public List<Article> getArticles() {
-		List<Article> articles = new ArrayList<>();
-		
-		Article article1 = new Article(1, "2021-01-23 20:00:00", "2021-01-23 20:00:00", "제목1", "내용1");
-		Article article2 = new Article(2, "2021-01-23 21:00:00", "2021-01-23 21:00:00", "제목2", "내용2");
-		Article article3 = new Article(3, "2021-01-23 22:00:00", "2021-01-23 22:00:00", "제목3", "내용3");
-		
-		articles.add(article1);
-		articles.add(article2);
-		articles.add(article3);
-		
-		return articles;
+	public List<Article> getArticles() {		
+		return articleDao.getArticles();
 	}
 }
